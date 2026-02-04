f6522cf55a182a7ff9f1c93bc15500950d3b0ee6
Ahmed <ahmed@example.com>
2026-02-04 06:46:55 +0400

    chore: add .gitignore and untrack build/install/log

--- build/ROAR_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/New_ROAR_Arm/src/ROAR_pkg
-# Build directory: /home/ahmed/New_ROAR_Arm/build/ROAR_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
